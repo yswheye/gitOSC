af368a30469c08c7a126dd63bcdae1b67aec998f
kymjs <kymjs123@gmail.com>
2015-11-30 17:09:50 +0800

    修改代码错误

--- gitoscandroid/src/main/java/net/oschina/gitapp/ui/CodeFileEditActivity.java
+++ gitoscandroid/src/main/java/net/oschina/gitapp/ui/CodeFileEditActivity.java
@@ -28,134 +28,136 @@ import cz.msebera.android.httpclient.Header;
 
 
 public class CodeFileEditActivity extends BaseActivity implements OnClickListener {
-	
-	private AppContext mAppContext;
-	
-	private CodeFile mCodeFile;
-	
-	private Project mProject;
-	
-	private String mPath;
-	
-	private String mBranch;
-	
-	private EditText mEditContent;
-	
-	private EditText mCommitMsg;
-	
-	private Button mCodeFilePub;
-	
-	private TextWatcher mTextWatcher = new TextWatcher() {
-		
-		@Override
-		public void onTextChanged(CharSequence s, int start, int before, int count) {
-			
-		}
-		
-		@Override
-		public void beforeTextChanged(CharSequence s, int start, int count,
-				int after) {
-			
-		}
-		
-		@Override
-		public void afterTextChanged(Editable s) {
-			if (mEditContent.getText().toString().equals(mCodeFile.getContent()) || StringUtils.isEmpty(mCommitMsg.getText().toString())) {
-				mCodeFilePub.setEnabled(false);
-			} else {
-				mCodeFilePub.setEnabled(true);
-			}
-		}
-	};
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_edit_codefile);
-		mAppContext = AppContext.getInstance();
-		initView();
-		initData();
-	}
-	
-	private void initView() {
-		mEditContent = (EditText) findViewById(R.id.codefile_edit);
-		mCommitMsg = (EditText) findViewById(R.id.codefile_edit_msg);
-		mCodeFilePub = (Button) findViewById(R.id.codefile_edit_pub);
-		
-		mEditContent.addTextChangedListener(mTextWatcher);
-		mCommitMsg.addTextChangedListener(mTextWatcher);
-		mCodeFilePub.setOnClickListener(this);
-	}
-	
-	private void initData() {
-		Intent intent = getIntent();
-		mCodeFile = (CodeFile) intent.getSerializableExtra(Contanst.CODE_FILE);
-		mProject = (Project) intent.getSerializableExtra(Contanst.PROJECT);
-		mPath = intent.getStringExtra(Contanst.PATH);
-		mBranch = intent.getStringExtra(Contanst.BRANCH);
-		if (mCodeFile != null) {
-			mEditContent.setText(mCodeFile.getContent());
-			mTitle = mCodeFile.getFile_name();
-			mSubTitle = mProject.getOwner().getName() + "/" + mProject.getName();
-		}
-	}
 
-	private void pubCommitCodeFile() {
-		final String content = mEditContent.getText().toString();
-		final String commit_message = mCommitMsg.getText().toString();
-		if (content.equals(mCodeFile.getContent())) {
-			UIHelper.ToastMessage(mAppContext, "文件内容没有改变");
-			return;
-		}
-		if (StringUtils.isEmpty(commit_message)) {
-			UIHelper.ToastMessage(mAppContext, "文件内容没有改变");
-			return;
-		}
+    private AppContext mAppContext;
+
+    private CodeFile mCodeFile;
+
+    private Project mProject;
+
+    private String mPath;
+
+    private String mBranch;
+
+    private EditText mEditContent;
+
+    private EditText mCommitMsg;
+
+    private Button mCodeFilePub;
+
+    private TextWatcher mTextWatcher = new TextWatcher() {
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (mEditContent.getText().toString().equals(mCodeFile.getContent()) || StringUtils
+                    .isEmpty(mCommitMsg.getText().toString())) {
+                mCodeFilePub.setEnabled(false);
+            } else {
+                mCodeFilePub.setEnabled(true);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_codefile);
+        mAppContext = AppContext.getInstance();
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        mEditContent = (EditText) findViewById(R.id.codefile_edit);
+        mCommitMsg = (EditText) findViewById(R.id.codefile_edit_msg);
+        mCodeFilePub = (Button) findViewById(R.id.codefile_edit_pub);
+
+        mEditContent.addTextChangedListener(mTextWatcher);
+        mCommitMsg.addTextChangedListener(mTextWatcher);
+        mCodeFilePub.setOnClickListener(this);
+    }
+
+    private void initData() {
+        Intent intent = getIntent();
+        mCodeFile = (CodeFile) intent.getSerializableExtra(Contanst.CODE_FILE);
+        mProject = (Project) intent.getSerializableExtra(Contanst.PROJECT);
+        mPath = intent.getStringExtra(Contanst.PATH);
+        mBranch = intent.getStringExtra(Contanst.BRANCH);
+        if (mCodeFile != null) {
+            mEditContent.setText(mCodeFile.getContent());
+            mTitle = mCodeFile.getFile_name();
+            mSubTitle = mProject.getOwner().getName() + "/" + mProject.getName();
+        }
+    }
+
+    private void pubCommitCodeFile() {
+        final String content = mEditContent.getText().toString();
+        final String commit_message = mCommitMsg.getText().toString();
+        if (content.equals(mCodeFile.getContent())) {
+            UIHelper.ToastMessage(mAppContext, "文件内容没有改变");
+            return;
+        }
+        if (StringUtils.isEmpty(commit_message)) {
+            UIHelper.ToastMessage(mAppContext, "文件内容没有改变");
+            return;
+        }
         final AlertDialog pubing = LightProgressDialog.create(this, "正在提交...");
 
 
-        GitOSCApi.updateRepositoryFiles(mProject.getId(), mBranch, mPath, mBranch, content, commit_message, new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                String msg = new String(responseBody);
-                if (msg != null) {
-                    GitViewUtils.showToast("提交成功");
-                    CodeFileEditActivity.this.finish();
-                } else {
-                    GitViewUtils.showToast("提交失败");
-                }
-            }
+        GitOSCApi.updateRepositoryFiles(mProject.getId(), mBranch, mPath, mBranch, content,
+                commit_message, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        if (responseBody != null && responseBody.length != 0) {
+                            GitViewUtils.showToast("提交成功");
+                            CodeFileEditActivity.this.finish();
+                        } else {
+                            GitViewUtils.showToast("提交失败");
+                        }
+                    }
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                GitViewUtils.showToast("提交失败");
-            }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody,
+                                          Throwable error) {
+                        GitViewUtils.showToast("提交失败");
+                    }
 
-            @Override
-            public void onFinish() {
-                super.onFinish();
-                pubing.dismiss();
-            }
+                    @Override
+                    public void onFinish() {
+                        super.onFinish();
+                        pubing.dismiss();
+                    }
 
-            @Override
-            public void onStart() {
-                super.onStart();
-                pubing.show();
-            }
-        });
-	}
+                    @Override
+                    public void onStart() {
+                        super.onStart();
+                        pubing.show();
+                    }
+                });
+    }
 
-	@Override
-	public void onClick(View v) {
-		int id = v.getId();
-		switch (id) {
-		case R.id.codefile_edit_pub:
-			pubCommitCodeFile();
-			break;
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.codefile_edit_pub:
+                pubCommitCodeFile();
+                break;
 
-		default:
-			break;
-		}
-	}
-	
+            default:
+                break;
+        }
+    }
+
 }
