c1688c2cc547ce08651a175d62d0d6f5cd76cc74
kymjs <kymjs123@gmail.com>
2015-12-30 11:42:16 +0800

    显著加快网络请求响应速度

--- gitoscandroid/src/main/java/net/oschina/gitapp/ui/fragments/ProjectIssuesFragment.java
+++ gitoscandroid/src/main/java/net/oschina/gitapp/ui/fragments/ProjectIssuesFragment.java
@@ -16,32 +16,34 @@ import java.util.List;
 
 /**
  * 项目issue列表
- * @created 2014-05-26
+ *
  * @author 火蚁（http://my.oschina.net/LittleDY）
- * 
- * 最后更新
- * 更新者
+ *         <p>
+ *         最后更新
+ *         更新者
+ * @created 2014-05-26
  */
 public class ProjectIssuesFragment extends BaseSwipeRefreshFragment<Issue> {
-	
-	private Project mProject;
 
-	public static ProjectIssuesFragment newInstance(Project project) {
-		ProjectIssuesFragment fragment = new ProjectIssuesFragment();
-		Bundle args = new Bundle();
-		args.putSerializable(Contanst.PROJECT, project);
-		fragment.setArguments(args);
-		return fragment;
-	}
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Bundle args = getArguments();
-		if (args != null) {
-			mProject = (Project) args.getSerializable(Contanst.PROJECT);
-		}
-	}
+    private Project mProject;
+
+    public static ProjectIssuesFragment newInstance(Project project) {
+        ProjectIssuesFragment fragment = new ProjectIssuesFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(Contanst.PROJECT, project);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+            mProject = (Project) args.getSerializable(Contanst.PROJECT);
+            requestData();
+        }
+    }
 
     @Override
     public CommonAdapter<Issue> getAdapter() {
@@ -58,10 +60,10 @@ public class ProjectIssuesFragment extends BaseSwipeRefreshFragment<Issue> {
         GitOSCApi.getProjectIssues(mProject.getId(), mCurrentPage, mHandler);
     }
 
-	@Override
-	public void onItemClick(int position, Issue issue) {
-		UIHelper.showIssueDetail(getActivity(), mProject, issue, null, null);
-	}
+    @Override
+    public void onItemClick(int position, Issue issue) {
+        UIHelper.showIssueDetail(getActivity(), mProject, issue, null, null);
+    }
 
     @Override
     protected String getEmptyTip() {
